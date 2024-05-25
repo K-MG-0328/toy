f7b0cbc0e5e617e384eed99927d33b8e57c5c5f9
kimmingyu94 <kmg3592@naver.com>
2024-05-25 23:44:50 +0900

    채팅 관련 설정

--- src/main/java/mg/toy/controller/ChatController.java
+++ src/main/java/mg/toy/controller/ChatController.java
@@ -4,9 +4,17 @@ import mg.toy.domain.ChatMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ChatController {
+
+    @GetMapping("/chat")
+    public String chatPage(Model model) {
+        return "chat/chat";
+    }
+
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public ChatMessage send(ChatMessage message) {
